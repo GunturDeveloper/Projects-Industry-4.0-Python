90ca93e5655596fd985d228cfc11763a2eea7a68
GunturDev <161219490+GunturDeveloper@users.noreply.github.com>
2024-08-03 13:37:11 +0700

    Create 7.1_Konsep_Dasar_OOP.py

---
+++ Belajar_Python/Bab_07_Pemrograman_Berorientasi_Objek/7.1_Konsep_Dasar_OOP.py
@@ -0,0 +1,11 @@
+# Konsep dasar OOP
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        return f"Hello, my name is {self.name} and I am {self.age} years old."
+
+person = Person("Guntur", 17)
+print(person.greet())
