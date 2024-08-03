274fd9244aae879a8c4556612720c52c0e8276aa
GunturDev <161219490+GunturDeveloper@users.noreply.github.com>
2024-08-03 13:28:49 +0700

    Create 2.1_Tipe_Data_Dasar.py

---
+++ Belajar_Python/Bab_02_Dasar_Dasar_Python/2.1_Tipe_Data_Dasar.py
@@ -0,0 +1,10 @@
+# Tipe data dasar di Python
+integer = 10       # Bilangan bulat
+floating = 10.5    # Bilangan pecahan
+string = "Hello"   # Tipe data string
+boolean = True     # Tipe data boolean
+
+print("Integer:", integer)
+print("Floating:", floating)
+print("String:", string)
+print("Boolean:", boolean)
